40367185fa09d90c9eff6fba6f5f10fe3e87d010
Leonard Velásquez <leonard.avp@gmail.com>
2023-01-16 16:51:16 -0400

    Despliegue de productos

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,14 +1,14 @@
-import { Button, Container } from "@mui/material"
+import { Button, Container, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material"
 import { useLiveQuery } from "dexie-react-hooks"
 import { useEffect, useState } from "react"
 import { db } from "../database/db"
-import {useNavigate} from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import ProductForm from "../components/ProductForm"
 
 const Products = () => {
 
-    const [query,setQuery] = useState('')
-    const [id,setId] = useState<undefined | number>(undefined)
+    const [query, setQuery] = useState('')
+    const [id, setId] = useState<undefined | number>(undefined)
     const [isDialogOpen, setIsDialogOpen] = useState(false)
     const [tasaDolar] = useState(+(localStorage.getItem('tasa') || 0))
 
@@ -16,17 +16,36 @@ const Products = () => {
 
     const clearQuery = () => setQuery('')
 
-    const productos = useLiveQuery(()=>db.productos.where('nombre').anyOf(query).toArray())
-    
+    const productos = useLiveQuery(() =>  query !== '' ? db.productos.where('nombre').anyOf(query).toArray() : db.productos.toArray()        )
+
     return (
         <>
             <Container>
-                <Container style={{display:'flex',flexDirection:'row',justifyContent:'space-evenly'}}>
-                    <Button onClick={()=>setIsDialogOpen(true)} variant="contained">Añadir producto</Button>
+                <Container style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-evenly' }}>
+                    <Button onClick={() => setIsDialogOpen(true)} variant="contained">Añadir producto</Button>
                     <Button variant="contained">Cambiar tasa del dolar</Button>
                 </Container>
+                <Container style={{marginTop:'1rem'}}>
+                    <TableContainer component={Paper}>
+                        <Table>
+                            <TableBody>
+                                {(productos && productos.length >0 ) && productos.map(producto => (
+                                    <TableRow
+                                        key={producto.id}
+                                    >
+                                        <TableCell component="th" scope="row" style={{fontWeight:'bold',fontSize:'1rem'}}>
+                                            {producto.nombre}
+                                        </TableCell>
+                                        <TableCell align="right">{producto.precioUnitario} $</TableCell>
+                                        <TableCell align="right">{producto.precioUnitario * tasaDolar} Bs</TableCell>
+                                    </TableRow>
+                                ))}
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+                </Container>
             </Container>
-            <ProductForm id={id} isOpen={isDialogOpen} onClose={()=>setIsDialogOpen(false)} isEdit={!!id}></ProductForm>
+            <ProductForm id={id} isOpen={isDialogOpen} onClose={() => setIsDialogOpen(false)} isEdit={!!id}></ProductForm>
         </>
     )
 }
